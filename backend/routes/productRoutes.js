d8aa502fce619fd8ed78c6bca5e86840efc27bde
Thitiwut <Thitiwut_suwannabud@cmu.ac.th>
2020-11-07 18:11:36 +0700

    productdetail and product

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -12,11 +12,10 @@ router.get(
   "/",
   asyncHandler(async (req, res) => {
     const products = await Product.find({});
+
     res.json(products);
   })
 );
-
-
 
 // @desc   Fetch single products
 // @route  GET /api/products/:id
@@ -29,10 +28,10 @@ router.get(
     if (product) {
       res.json(product);
     } else {
-      res.status(404)
-      throw new Error('Product not found')
+      res.status(404);
+      throw new Error("Product not found");
     }
   })
 );
 
-export default router
+export default router;
