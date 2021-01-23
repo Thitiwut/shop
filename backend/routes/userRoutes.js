1d55422f90c20f6e7b51cd701e68c1a5000612b3
Thitiwut <Thitiwut_suwannabud@cmu.ac.th>
2021-01-23 11:19:53 +0700

    banky

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,11 +2,13 @@ import express from "express";
 
 const router = express.Router();
 
-import { authUser, getUserProfile , registerUser } from "../controller/userController.js";
+import { authUser, getUserProfile, registerUser, updateUserProfile } from "../controller/userController.js";
 import { protect } from '../middleware/authMiddleware.js'
 
 router.route('/').post(registerUser)
-router.post('/login',authUser)
-router.route("/profile").get(protect,getUserProfile);
-
+router.post('/login', authUser)
+router
+  .route("/profile")
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
 export default router;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,10 @@ import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from './screens/CartScreen';
 import LoginScreen from "./screens/LoginScreen";
+import RegisterScreen from "./screens/RegisterScreen";
+import ProfileScreen from "./screens/ProfileScreen";
+import ShippingScreen from "./screens/ShippingScreen";
+
 function App() {
   return (
     <Router>
@@ -16,6 +20,10 @@ function App() {
       <main className="py-3">
         <Container>
           <Route path="/login" component={LoginScreen} />
+          <Route path="/shipping" component={ShippingScreen} />
+
+          <Route path="/register" component={RegisterScreen} />
+          <Route path="/profile" component={ProfileScreen} />
           <Route path="/product/:id" component={ProductScreen} />
           <Route path="/cart/:id?" component={CartScreen} />
           <Route path="/" component={HomeScreen} exact />
